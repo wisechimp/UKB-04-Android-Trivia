50fd92cfa068d576cf7d0cadf45e6809c3769795
shicks1080 <stuart.hicks@gmail.com>
2019-08-27 11:04:09 +0200

    Adding the navigation graph and connecting the title to the game screen

--- app/src/main/java/com/wisechimp/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/wisechimp/androidtrivia/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.wisechimp.androidtrivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -16,6 +17,8 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 }
