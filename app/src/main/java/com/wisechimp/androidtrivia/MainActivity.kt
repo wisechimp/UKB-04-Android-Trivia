af8c470dea7a74dd12ce7578021d3999c34cf01d
shicks1080 <stuart.hicks@gmail.com>
2019-08-27 10:48:31 +0200

    Imported the project's files from Udacity.
    Added a title fragment and... it's alive!

--- app/src/main/java/com/wisechimp/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/wisechimp/androidtrivia/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+   }
 }
